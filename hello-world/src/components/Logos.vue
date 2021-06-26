5a351d0345a9c13b9e3cfee94c2a3c9c19753b3e
Randyfp2 <randyfp2@gmail.com>
2021-06-26 20:25:03 +0700

    Membuat Komponen baru
    Membuat komponen baru dan di tampilkan keapp.js

---
+++ hello-world/src/components/Logos.vue
@@ -0,0 +1,26 @@
+<template>
+<br>
+<div>
+    <h2>Text ini dimunculkan dari komponen baru</h2>
+</div>
+    
+</template>
+
+<script>
+export default {
+    name:'app',
+}
+</script>
+
+<style scoped>
+div{
+    align-items: center;
+    padding-top: 5px;
+    padding-bottom: 5px;
+    background-color: gray;
+}
+h2 {
+    color:whitesmoke;
+}
+
+</style>
